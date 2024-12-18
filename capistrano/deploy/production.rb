9855894a54e0adc639b11d2ca0c16d828454e1ac
Colin Polonowski <colinpolonowski@Colins-MacBook-Pro-2.local>
2024-12-18 13:00:22 +0000

    Initial commit

---
+++ capistrano/deploy/production.rb
@@ -0,0 +1,2 @@
+server '139.144.144.87',       port: 22, user: 'deploy', roles: %w{nginx}                                      # Code Server - ALL SITES
+set :deploy_to, '/var/www/dungeon.croftsoftsoftware.com'
